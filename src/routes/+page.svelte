c16a1e91738d7962a9a6fadb1e2ef5fe5f3bc734
SkuliX01 <skulix137@proton.me>
2023-10-18 12:44:25 +0200

    Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import BiHomeAlt2 from "svelte-icons-pack/bi/BiHomeAlt2";
+    import Icon from 'svelte-icons-pack/Icon.svelte';
+
+</script>
+
+<div class="flex justify-content-center justify-center">
+    <div class="flex justify-content-center justify-center bg-white w-[240px] h-[55px] mt-[15px] rounded-xl">
+        
+    </div>
+</div>
+
+
+<style lang="postcss">
+
+</style>
